8e68dcdfa3cb931bd07c7e05c0da89e2cf3ccb32
Ashish Kandu <ashishkandu43@gmail.com>
2023-08-24 09:41:44 +0545

    fix typo

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -52,7 +52,7 @@ class PrivateTagsApiTests(TestCase):
 
         tags = Tag.objects.all().order_by('-name')
         serializer = TagSerializer(tags, many=True)
-        self.assertEqual(res.staus_code, status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
     def test_tags_limited_to_user(self):
